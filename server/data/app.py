935b2f45ace897e3c01e07da73185b8682fd56a1
kavin033 <rk.kavin2003@gmail.com>
2023-02-12 07:56:07 +0530

    changes

--- server/data/app.py
+++ server/data/app.py
@@ -2,6 +2,7 @@ from flask import Flask,request,render_template,redirect,session,jsonify
 import requests
 import sqlite3
 import hashlib
+import os
 
 con = sqlite3.connect('test.db')
 cursor = con.cursor()
@@ -223,5 +224,6 @@ def getUser():
     except:
          return jsonify({"data": False})
 
-if __name__ == '__main__':
-    app.run(debug = True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
